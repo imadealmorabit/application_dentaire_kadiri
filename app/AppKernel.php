56c2387fbc5e4b5c14719641d70d58cd5ca30ee1
imadealmorabit <imadealmorait@gmail.com>
2018-01-10 22:10:27 +0000

    finalisation de FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Kadiri\KadiriBundle\KadiriBundle(),
-            //new FOS\UserBundle\FOSUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Utilisateurs\UtilisateursBundle\UtilisateursBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
